86a77018b19301c52e2e870135660b875cf6aaa5
zigzi salah eddine <s.zigzi@gmail.com>
2019-05-24 02:27:19 +0000

    add front-end page to consume restapi

--- src/main/java/com/zigdev/app/employees/EmployeeResource.java
+++ src/main/java/com/zigdev/app/employees/EmployeeResource.java
@@ -5,6 +5,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+
 @Path( "/employees" )
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
